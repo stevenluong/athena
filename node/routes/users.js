766c9086208aba73b4095192a1bbc233bdfebf94
ste.luong@gmail.com <ste.luong@gmail.com>
2020-08-12 09:48:59 +0930

    User fix

--- node/routes/users.js
+++ node/routes/users.js
@@ -32,8 +32,8 @@ router.post('/', (req, res, next) => {
   else {
     token = "00_E79okIopZ9SlSid6B-PCgiaU-AUDRQW1MQlIoi4";
   }
-  */
   console.log(token);
+  */
   const client = new okta.Client({
     orgUrl: 'https://dev-607424.okta.com',
     token: "00Vjq7M1ZjOoKwf77dR0kiShSWNmfurQBXhdLFzWIk"
